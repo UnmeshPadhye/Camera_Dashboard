d4ec72c107ab740aed2c14c9a81a3aa30b83001b
Unmesh Padhye <unmeshpadhye@gmail.com>
2023-04-11 20:42:30 -0700

    Alerts Page and Route added

---
+++ frontend/camera-dash/src/components/alerts/alertsPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AlertsPage() {
+    return (
+        <div className="container">
+            This is Alerts Page
+        </div>
+    );
+}
+
+export default AlertsPage;
